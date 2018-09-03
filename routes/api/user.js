77c60bff852759d4b77bad6fabd0941cdf80eafa
tm-315 <topguru315@gmail.com>
2018-09-02 20:55:02 -0500

    /user PATCH successfully edits only the key value that is inputted in req.body

--- routes/api/user.js
+++ routes/api/user.js
@@ -5,45 +5,60 @@ const db = require('../../db')
 
 router.get('/', function(req, res) {
 	db.select()
-		.from('users')
-		.orderBy('id')
-		.then(function(data) {
-			res.send(data)
-		})
+	.from('users')
+	.orderBy('id')
+	.then(function(data) {
+		res.send(data)
+	})
 })
 
-		
 // body-parser lets server understand what your POSTed data.
 // INSERT INTO 'tableName"(col1, col2) VALUES (col1_value, col2_value);
 // SELECT * FROM TABLE WHERE ID = inserted_row;
 router.post('/', (req, res) => {
 	db.insert(req.body)
-		.returning('*')
-		.into('users')
-		.then((data) => {
+	.returning('*')
+	.into('users')
+	.then((data) => {
+		res.send(data)
+	})
+})
+
+router.get('/:id', (req, res) =>
+	db('users')
+		.where({id: req.params.id })
+		.first()
+		.then(function(data) {
 			res.send(data)
 		})
-})
+)
 
 // IDEMOPOTENCE: is gonna be the say no matter how many times you call on something. (GET, POST)
 
 // PATCH only modifies the one we submit in the body.
-// router.patch('/:id', function(req, res) {
-// 	db('todo').where({ id: req.params.id })
-// 		.update(req.body).returning('*').then(function(data) {
-// 			res.send(data)
-// 		})
-// })
+router.patch('/:id', function(req, res) {
+	db('users')
+		.where({ id: req.params.id })
+		.update(req.body)
+		.returning('*')
+		.then(function(data) {
+			res.send(data)
+		})
+})
 
 // // PUT should replace everything with whats in body + replacing the rest with null.
-// router.put('/:id', function(req,res) {
-// 	db('todo').where({ id: req.params.id }).update({
-// 		title: req.body.title || null,
-// 		is_done: req.body.is_done || null
-// 	}).returning('*').then(function(data) {
-// 		res.send(data)
-// 	})
-// })
+router.put('/:id', function(req,res) {
+	db('users')
+		.where({ id: req.params.id })
+		.update({
+			title: req.body.title || null,
+			is_done: req.body.is_done || null
+		})
+		.returning('*')
+		.then(function(data) {
+		res.send(data)
+	})
+})
 
 // // Delete pretty easy.
 // router.delete(':/id', function(req,res) {
@@ -53,15 +68,6 @@ router.post('/', (req, res) => {
 // 		})
 // })
 
-// //db(databaseName)
-// router.get('/:id', function(req, res) {
-// 	db('todo')
-// 		.where({id: req.params.id })
-// 		.first()
-// 		.then(function(data) {
-// 			res.send(data)
-// 		})
-// })
 
 
 module.exports = router
