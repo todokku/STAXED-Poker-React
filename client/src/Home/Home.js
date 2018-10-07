851b848ffa476fc12d860123aaa5ab56c5a693fc
tm-315 <topguru315@gmail.com>
2018-10-08 01:23:43 +0800

    first commit by orrin

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -3,6 +3,7 @@ import "../Profile/Profile.css";
 
 import ReactDom from "react-dom";
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import posed from 'react-pose';
 // import posed, { PoseGroup } from "react-pose";
 // (4) Sub components for Animations
 // import PokerNews from "./PokerNews";
@@ -15,48 +16,71 @@ import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 //   exit: { opacity: 0 }
 // });
 
+const ButtonContainer = posed.div({
+  open: {
+    x: '0%',
+    delayChildren: 200,
+    staggerChildren: 50
+  },
+  closed: { x: '-100%', delay: 300 }
+});
+
+const Button = posed.div({
+  open: { y: 0, opacity: 1 },
+  closed: { y: 20, opacity: 0 }
+});
+
 class Home extends Component {
+  state = { isOpen: false };
+  
+  componentDidMount() {
+    setTimeout(this.toggle, 1000);
+  }
+
+  toggle = () => this.setState({ isOpen: !this.state.isOpen });
+
   render(props) {
+    const { isOpen } = this.state;
     return (
       <div id="container">
-        <div id="content-container">
-          <div className="button-top">
+        <ButtonContainer id="content-container" pose={isOpen ? 'open' : 'closed'}>
+          <Button className="button-top">
             <button type="button" className="block">
               Poker News - 
             </button>
-          </div>
+          </Button>
 
-          <div className="container-center">
+          <Button className="container-center">
             <h3>San Antonio's Coolest Private Club</h3>
             <h1>RIVER CITY</h1>
             <br />
             <h2>CARD ROOM</h2>
             <h5>Open 7 days a week for good times!</h5>
             <h4>Poker • Arcade & Console Games • Chess</h4>
-          </div>
+          </Button>
 
           <div className="rotate-left">
-            <div className="button-left">
+            <Button className="button-left">
               <button type="button" className="block">
                 Club Information 
               </button>
-            </div>
+            </Button>
           </div>
 
           <div className="rotate-right">
-            <div className="button-right">
+            <Button className="button-right">
               <button type="button" className="block">
                 Contact - 
               </button>
-            </div>
+            </Button>
           </div>
 
-          <div className="button-bottom">
+          <Button className="button-bottom">
             <button type="button" className="block">
               Club Calendar - 
             </button>
-          </div>
-        </div>
+          </Button>
+        </ButtonContainer>
       </div>
     );
   }
